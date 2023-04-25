d05944a7f64b40071293338c3c4226c5079d420b
Paul Hammant <paul@hammant.org>
2023-04-25 21:09:06 +0100

    filter

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
+  testMatch: ["<rootDir>/tests/unit/*.spec.js"],
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': 'vue3-jest',
